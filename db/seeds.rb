814ae9aceca5d2dd3d5c922104f8fcea24b00e51
ispacowanguitar <ispacowanguitar@gmail.com>
2016-03-19 01:36:34 -0500

    added seeds back with user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-# ['admin', 'customer'].each do |role|
-#   Role.find_or_create_by(name: role)
-# end
+['admin', 'customer'].each do |role|
+  Role.find_or_create_by(name: role)
+end
 
+admin = User.new(email: "admin@launchpadtheaters.com", password: 'password', password_confirmation: 'password', role_id: 1)
+
+unless User.find_by(email: "admin@launchpadtheaters.com")
+ admin.save
+end
